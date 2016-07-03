166174c3cebaf718c5e74c8d5b3fd1a061c0bb3c
Ahmed Emad Mohamed <egytech2013@hotmail.com>
2016-07-03 14:30:08 +0200

    Create main template and data module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [ SidebarComponent ]
 })
 export class AppComponent {
   title = 'app works!';
